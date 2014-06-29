d68ed7a0133c744458565b737baf46c4528a3ce9
ashok-sv <ashok.s.vydyanathan@gmail.com>
2014-06-29 13:45:51 -0700

    Added Python code

---
+++ Titanic/Kaggle-Titanic/run1.py
@@ -0,0 +1,32 @@
+# importing required packages
+import csv as csv
+import numpy as np
+
+# importing data and populating array
+csv_file_object = csv.reader(open('../train.csv', 'rb'))
+header = csv_file_object.next()
+data = []
+for row in csv_file_object:
+    data.append(row)
+data = np.array(data)
+
+# quick check that data is correct
+print data
+
+# working out survival proportion
+number_passengers = np.size(data[0::, 1].astype(np.float))
+number_survived = np.sum(data[0::, 1].astype(np.float))
+proportion_survived = number_survived / number_passengers
+print "Proportion of survivors: " + str(proportion_survived)
+
+# basic gender filtering
+women_only_stats = data[0::, 4] == "female"
+men_only_stats = data[0::, 4] == "male"
+
+women_onboard = data[women_only_stats, 1].astype(np.float)
+men_onboard = data[men_only_stats, 1].astype(np.float)
+proportion_women_survived = np.sum(women_onboard) / np.size(women_onboard)
+proportion_men_survived = np.sum(men_onboard) / np.size(men_onboard)
+
+print("Proportion of women who survived: " + str(proportion_women_survived))
+print("Proportion of men who survived: " + str(proportion_men_survived))
